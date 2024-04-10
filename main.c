6432fa2c3f2174200c85b5a31faf7f479a88efd7
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2024-04-10 15:48:39 +0200

    [TEST] valgrind test for the workflow

--- main.c
+++ main.c
@@ -6,10 +6,16 @@
 */
 
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int k;
-    k[5] = 9;
+    int i;
+    char *str = malloc(sizeof(char) * 1000);
+    str = strdup("Hello World !\n");
+    write(1, str, strlen(str));
+    printf("i = %d\n", i);
     return 0;
 }
