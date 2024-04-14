fa212fa8144c5e70516ce20d1b2568f35ff17312
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2024-04-14 22:16:44 +0200

    [TEST] main to test replit environnement

--- main.c
+++ main.c
@@ -6,11 +6,25 @@
 */
 
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
+    char *token = NULL;
+    size_t len = 0;
+
+    while (1) {
+        if (getline(&token, &len, stdin) == -1) {
+            free(token);
+            return 0;
+        }
+        if (strcmp(token, "exit\n") == 0) {
+            free(token);
+            return 0;
+        }
+        printf("%s", token);    
+    }
+    free(token);
     return 0;
 }
