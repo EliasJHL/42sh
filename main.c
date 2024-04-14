c46b26829a6f03c041918ae64906ffd01d833eb7
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2024-04-14 22:23:51 +0200

    [DELETE] test main deleted to not have banned function used

--- main.c
+++ main.c
@@ -11,20 +11,5 @@
 
 int main(void)
 {
-    char *token = NULL;
-    size_t len = 0;
-
-    while (1) {
-        if (getline(&token, &len, stdin) == -1) {
-            free(token);
-            return 0;
-        }
-        if (strcmp(token, "exit\n") == 0) {
-            free(token);
-            return 0;
-        }
-        printf("%s", token);    
-    }
-    free(token);
     return 0;
 }
